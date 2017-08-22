a48aeca9f7397429419303a15671a5214c93c266
woobin huh <madgb89@naver.com>
2017-08-22 14:04:52 +0900

    no message

---
+++ js/schedule.js
@@ -0,0 +1,11 @@
+(document).ready(function($) {
+	$('.event').click(function(){
+        var theDate = $(this).text();
+        $('.date-s').text(theDate);
+        if(theDate == 11){
+          $('.scd01').show().siblings().hide();
+        }else if(theDate == 22){
+          $('.scd02').show().siblings().hide();
+        }
+    });
+});
