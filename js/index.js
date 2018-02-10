4478d5c8d1b48c7e7b0166eb026b0a7195d3af75
woobin huh <madgb89@naver.com>
2018-02-11 00:00:29 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -10,17 +10,31 @@ $(document).ready(function() {
 	$('.navigation').on('click', '.home-li', function(e) {
 		e.preventDefault();
         $.ajax('home.html', {
-          success: function(response) {
-            $('.main').html(response);
-          }
+    		timeout: 3000,	
+	        success: function(response) {
+	        	$('.main').html(response);
+	        },
+	        beforeSend: function() {
+            	$('.main').addClass('loading');
+          	},
+          	complete: function() {
+            	$('.main').removeClass('loading');
+          	}
         });
   	});
   	$('.navigation').on('click', '.albums-li', function(e) {
 		e.preventDefault();
         $.ajax('albums.html', {
-          success: function(response) {
-            $('.main').html(response);
-          }
+        	timeout: 3000,
+        	success: function(response) {
+            	$('.main').html(response);
+          	},
+          	beforeSend: function() {
+            	$('.main').addClass('loading');
+	      	},
+	      	complete: function() {
+	        	$('.main').removeClass('loading');
+	      	}
         });
   	});
 });
